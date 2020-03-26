8d713b0bffb2fc3968d69ceb5c8ed1702ba8294c
Willis Aybar <willisaybar@gmail.com>
2020-03-26 02:02:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX(color) FROM bears COUNT(color)"
+  "SELECT MAX(color) FROM bears ASC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
