07753317df5744839a4204dc2bc985c9a8bd5ff5
Willis Aybar <willisaybar@gmail.com>
2020-03-26 00:56:36 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "male", "brown", "angry", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Rowdy", 93, "male", "grey", "smiley", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Tabitha", 62, "female", "orange", "happy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 45, "male", "black", "mad", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Melissa", 20, "female", "pink", "lovely", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Grinch", 14, "female", "blue", "ok", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Wendy", 28, "female", "black", "happy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 10, "male", "brown", "angry", 0);
